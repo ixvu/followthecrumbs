4280f8985c5e6189f5f7a4c7241a78d27afc69a5
Venkatesh <venkatesh@indix.com>
2016-11-21 15:57:16 +0530

    error handling and improvements

--- src/main/scala/com/indix/ml/analysis/breadcrumbs/BreadCrumbAnalyzer.scala
+++ src/main/scala/com/indix/ml/analysis/breadcrumbs/BreadCrumbAnalyzer.scala
@@ -1,17 +1,27 @@
 package com.indix.ml.analysis.breadcrumbs
 
 import org.apache.spark.sql.SparkSession
-
+import org.apache.log4j.{Level,Logger}
 
 object BreadCrumbAnalyzer {
   def main(args: Array[String]) {
-    val breadCrumbsFile = "/home/vumaasha/Downloads/StoreWiseBreadCrumbStats.json"
+    val logger = Logger.getLogger(this.getClass.getName)
+    logger.setLevel(Level.INFO)
+    val inputFile = args(0)
+    val outputFile = args(1)
+    val breadCrumbsFile = inputFile
+    logger.info(s"The inputfile is $inputFile")
+    logger.info(s"The output file is $outputFile")
     val spark = SparkSession.builder().appName("BreadCrumbAnalyzer").getOrCreate()
     val breadCrumbsDs = spark.read.json(breadCrumbsFile)
-    breadCrumbsDs.createOrReplaceTempView("store_bc")
+    breadCrumbsDs.repartition(breadCrumbsDs("storeId")).cache().createOrReplaceTempView("store_bc")
     spark.udf.register("tokenize", (doc: String) => WordNetTokenizer.tokenize(doc))
-    val tokenDF = spark.sql("select distinct storeId,token from ( select storeId,explode(tokenize(breadCrumbs)) as token from store_bc) as x limit 10")
-    tokenDF.show(false)
+    val tokenDF = spark.sql("" +
+      "select " +
+      "   storeId,storeName, collect_set(token) as tokens from " +
+      "( select storeId,storeName, explode(tokenize(breadCrumbs)) as token from store_bc) as x" +
+      " group by storeId,storeName")
+    tokenDF.coalesce(1).write.option("compression", "gzip").mode("overwrite").json(outputFile)
     spark.stop()
   }
 
--- src/main/scala/com/indix/ml/analysis/breadcrumbs/WordNetTokenizer.scala
+++ src/main/scala/com/indix/ml/analysis/breadcrumbs/WordNetTokenizer.scala
@@ -8,11 +8,14 @@ import edu.mit.jwi.item.POS
 import edu.mit.jwi.morph.WordnetStemmer
 
 import scala.collection.JavaConverters._
+import org.apache.log4j.{Level,Logger}
 
 /**
   * Created by vumaasha on 18/11/16.
   */
 object WordNetTokenizer {
+  val logger = Logger.getLogger(this.getClass.getName)
+  logger.setLevel(Level.INFO)
   val wordNetDir = "/opt/WordNet-3.0/dict"
   val dict = new RAMDictionary(new File(wordNetDir))
   dict.setLoadPolicy(ILoadPolicy.IMMEDIATE_LOAD)
@@ -20,14 +23,23 @@ object WordNetTokenizer {
   val stemmer = new WordnetStemmer(dict)
 
   def tokenize(doc: String): Array[String] = {
-    val token_pattern = "\\b\\w\\w+\\b".r
-    token_pattern.findAllIn(doc.toLowerCase).flatMap((token) => {
-      stemmer.findStems(token, POS.NOUN)
-    }.asScala).toSet.toArray
+    try {
+      val token_pattern = "\\b\\w\\w+\\b".r
+      token_pattern.findAllIn(doc.toLowerCase).flatMap((token) => {
+        stemmer.findStems(token, POS.NOUN)
+      }.asScala).toSet.toArray
+    } catch {
+      case e: Throwable  => {
+        logger.info(s"The errored document is $doc")
+        logger.info(e)
+        Array.empty
+      }
+    }
   }
 
   def main(args: Array[String]): Unit = {
     val tokens = tokenize("\"Home > Watches > Ice-Watch Watches > Ice-Watch Unisex Watches > Ice-Pure")
+    tokenize("")
     tokens.foreach(println(_))
   }
 }
