8d95b10610d930795b426008c2262aa4e236a59f
Venkatesh <venkatesh@indix.com>
2016-11-29 18:05:09 +0530

    added coefficients

--- src/main/scala/com/indix/ml/TopLevelModel.scala
+++ src/main/scala/com/indix/ml/TopLevelModel.scala
@@ -21,42 +21,76 @@ class TopLevelModel(modelPath: String) {
     json
   }
 
-  val modelJsValue:JValue = {
+  val modelJsValue: JValue = {
     val modelJson = readJsonFromResource(modelPath)
     parse(modelJson)
   }
 
-  val categoryMapping: Map[Int,String] = {
+  val categoryMapping: Map[Int, String] = {
     val tuples = for {
       JObject(child) <- modelJsValue
       JField("class_names", JObject(classNames)) <- child
-      JField((a:String,JString(b:String))) <- classNames
+      JField((a: String, JString(b: String))) <- classNames
     } yield (a.toInt, b)
     Map(tuples: _*)
   }
 
-  val categoryIds: Map[Int,Int] = {
+  val categoryIds: Map[Int, Int] = {
     val tuples = for {
       JObject(child) <- modelJsValue
       JField("classes", JObject(classNames)) <- child
-      JField((a:String,JInt(b))) <- classNames
+      JField((a: String, JInt(b))) <- classNames
     } yield (a.toInt, b.toInt)
     Map(tuples: _*)
   }
 
-  def getCategory(id:Int) = categoryMapping(categoryIds(id))
+  def getCategory(id: Int) = categoryMapping(categoryIds(id))
 
-  val intercept:DenseVector[Double] = {
-     val intercepts = for {
-       JObject(child) <- modelJsValue
-       JField("intercept",JArray(intList)) <- child
-       JDouble(b) <- intList
-     } yield b
-    DenseVector(intercepts:_*)
+  val intercept: DenseVector[Double] = {
+    val intercepts = for {
+      JObject(child) <- modelJsValue
+      JField("intercept", JArray(intList)) <- child
+      JDouble(b) <- intList
+    } yield b
+    DenseVector(intercepts: _*)
+  }
+
+  val classSparsity: List[Int] = {
+    for {
+      JObject(child) <- modelJsValue
+      JField("coefficients", JObject(coeff)) <- child
+      JField(clsId, JObject(clsCoeff)) <- coeff
+      JField("size", JInt(size)) <- clsCoeff
+    } yield size.toInt
+  }
+
+  val vocabulary = {
+    val vocabTokens: Seq[(String, Int)] = for {
+      JObject(child) <- modelJsValue
+      JField("vocabulary", JObject(features)) <- child
+      JField(word, JInt(index)) <- features
+    } yield (word, index.toInt)
+    Map(vocabTokens: _*)
+  }
+
+
+  val coefficients: Seq[SparseVector[Double]] = {
+    for {
+      JObject(child) <- modelJsValue
+      JField("coefficients", JObject(coeff)) <- child
+      JField(clsId, JObject(clsInfo)) <- coeff
+      JField("class_coefficients", JObject(clsCoeff)) <- clsInfo
+    } yield {
+      val tuples = for {
+        JField(index, JDouble(featureCoeff)) <- clsCoeff
+      } yield (index.toInt, featureCoeff)
+      SparseVector(vocabulary.size)(tuples: _*)
+    }
   }
 }
 
 object TopLevelModel {
+
   def apply(modelPath: String): TopLevelModel = new TopLevelModel(modelPath)
 
   def apply(): TopLevelModel = new TopLevelModel("TopLevelModel.json")
